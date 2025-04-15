6ca24d616a81cf82d08637cf11d82f6d5e5aea40
Tin Man <tinmanlessons@gmail.com>
2025-04-15 15:15:10 -0500

    Added Debug and Error logging

--- 8Bells.sh
+++ 8Bells.sh
@@ -2,35 +2,53 @@
 
 mpv_cmd="/usr/bin/mpv --ao=jack"
 quiet=">/dev/null 2>&1"
+error_log="/path/to/logger_8bells_error.log"
+debug_log="/path/to/logger_8bells_debug.log"
 
 current_time=$(date +%H%M)
 
+log_debug() {
+    echo "$(date +'%Y-%m-%d %H:%M:%S') [DEBUG] $1" >> "$debug_log"
+}
+
+log_error() {
+    echo "$(date +'%Y-%m-%d %H:%M:%S') [ERROR] $1" >> "$error_log"
+}
+
 case "$current_time" in
     0800|1200|1600|2000)
-        ${mpv_cmd} /home/coder/Music/8Bells/8b.mp3 $quiet
+        file="/home/coder/Music/8Bells/8b.mp3"
         ;;
     0830|1230|1630)
-        ${mpv_cmd} /home/coder/Music/8Bells/1b.mp3 $quiet
+        file="/home/coder/Music/8Bells/1b.mp3"
         ;;
     0900|1300|1700)
-        ${mpv_cmd} /home/coder/Music/8Bells/2b.mp3 $quiet
+        file="/home/coder/Music/8Bells/2b.mp3"
         ;;
     0930|1330|1730)
-        ${mpv_cmd} /home/coder/Music/8Bells/3b.mp3 $quiet
+        file="/home/coder/Music/8Bells/3b.mp3"
         ;;
     1000|1400|1800)
-        ${mpv_cmd} /home/coder/Music/8Bells/4b.mp3 $quiet
+        file="/home/coder/Music/8Bells/4b.mp3"
         ;;
     1030|1430|1830)
-        ${mpv_cmd} /home/coder/Music/8Bells/5b.mp3 $quiet
+        file="/home/coder/Music/8Bells/5b.mp3"
         ;;
     1100|1500|1900)
-        ${mpv_cmd} /home/coder/Music/8Bells/6b.mp3 $quiet
+        file="/home/coder/Music/8Bells/6b.mp3"
         ;;
     1130|1530|1930)
-        ${mpv_cmd} /home/coder/Music/8Bells/7b.mp3 $quiet
+        file="/home/coder/Music/8Bells/7b.mp3"
         ;;
     *)
         exit 0
         ;;
 esac
+
+log_debug "Attempting to play: $file"
+
+if ${mpv_cmd} "$file" $quiet; then
+    log_debug "Successfully played: $file"
+else
+    log_error "Failed to play: $file"
+fi
