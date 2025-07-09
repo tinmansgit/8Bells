b3d3e1507a151eceb37594d78e297e5ae5eacd00
Tin Man <tinmanlessons@gmail.com>
2025-07-09 11:15:57 -0500

    Update

---
+++ 8Bells.sh
@@ -0,0 +1,57 @@
+#!/usr/bin/env bash
+
+mpv_cmd="/usr/bin/mpv --ao=jack"
+quiet=">/dev/null 2>&1"
+error_log="/home/coder/bin/Bash/8Bells/logger_8bells_error.log"
+debug_log="/home/coder/bin/Bash/8Bells/logger_8bells_debug.log"
+
+current_time=$(date +%H%M)
+
+log_debug() {
+    echo "$(date +'%Y-%m-%d %H:%M:%S') [DEBUG] $1" >> "$debug_log"
+	}
+
+log_error() {
+    echo "$(date +'%Y-%m-%d %H:%M:%S') [ERROR] $1" >> "$error_log"
+	}
+
+log_debug "checking time"
+
+case "$current_time" in
+    0800|1200|1600|2000)
+        file="/home/coder/Music/8Bells/8b.mp3"
+        ;;
+    0830|1230|1630)
+        file="/home/coder/Music/8Bells/1b.mp3"
+        ;;
+    0900|1300|1700)
+        file="/home/coder/Music/8Bells/2b.mp3"
+        ;;
+    0930|1330|1730)
+        file="/home/coder/Music/8Bells/3b.mp3"
+        ;;
+    1000|1400|1800)
+        file="/home/coder/Music/8Bells/4b.mp3"
+        ;;
+    1030|1430|1830)
+        file="/home/coder/Music/8Bells/5b.mp3"
+        ;;
+    1100|1500|1900)
+        file="/home/coder/Music/8Bells/6b.mp3"
+        ;;
+    1130|1530|1930)
+        file="/home/coder/Music/8Bells/7b.mp3"
+        ;;
+    *)
+        log_debug "Finished time check"
+        exit 0
+        ;;
+esac
+
+log_debug "Attempting to play: $file"
+
+if eval "${mpv_cmd} \"$file\" $quiet"; then
+    log_debug "Successfully played: $file"
+else
+    log_error "Failed to play: $file"
+fi
